f23642bb20806d6fdab36c1e81ca6f5059bec836
aswankotalh <74552374+aswankotalh@users.noreply.github.com>
2023-06-15 10:35:47 +0200

    catboy sample dialogue switches in console

--- src/js/catboy.js
+++ src/js/catboy.js
@@ -1,49 +1,64 @@
-import { Actor, Random, Vector, Timer } from "excalibur";
+import { Actor, Random, Vector, Timer,Input } from "excalibur";
 import { Resources, ResourceLoader } from "./resources.js";
 
 
 export class Catboy extends Actor {
-
+    index = 0
     game;
     constructor() {
         super({
-            width: Resources.Sans.width,
-            height: Resources.Sans.height
+            // width: Resources.Sans.width,
+            // height: Resources.Sans.height
         });
         // const playerOne = new Actor()
-        this.graphics.use(Resources.Sans.toSprite());
-        this.pos = new Vector(585, 200);
-        this.scale = new Vector(0.1, 0.1);
+        // this.graphics.use(Resources.Sans.toSprite());
+        // this.pos = new Vector(585, 200);
+        // this.scale = new Vector(0.1, 0.1);
 
         
-        this.random = new Random(1337)
+        // this.random = new Random(1337)
 
     }
     onInitialize(engine) {
- 
+        this.game = engine;
 
     }
 
-
-
-    dialogueOptions() {
-        console.log("switch");
+    startDialogue(){
+        let jsontext ={"character 1":["THATS RIGHT,ACE ATTORNEY IN 7 LANGUAGES","test2","test3"],
+    }
     
-        let dodgeBullet = this.random.integer(0, 2)
+    let char1text = jsontext["character 1"]
+   
+    console.log(char1text[this.index])
+    }
+    
 
-        switch (dodgeBullet) {
-            case 0:
-                this.actions.moveTo (new Vector(585, 50),200);
-                break;
-            case 1: 
-                this.actions.moveTo (new Vector(585, 200),200);
-                break;
-            case 2: 
-                this.actions.moveTo (new Vector(585, 400),200);
-                break;
-            default:
-                break;
+    onPreUpdate(engine){
+        if (engine.input.keyboard.wasPressed(Input.Keys.Space)) {
+            this.index++
+            this.startDialogue()
         }
     }
+
+    // dialogueOptions() {
+    //     console.log("switch");
+    
+    //     let dodgeBullet = this.random.integer(0, 2)
+
+    //     switch (dodgeBullet) {
+    //         case 0:
+    //             this.actions.moveTo (new Vector(585, 50),200);
+    //             break;
+    //         case 1: 
+    //             this.actions.moveTo (new Vector(585, 200),200);
+    //             break;
+    //         case 2: 
+    //             this.actions.moveTo (new Vector(585, 400),200);
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
 
 }
--- src/js/scenes/gamescene.js
+++ src/js/scenes/gamescene.js
@@ -1,16 +1,37 @@
 import { Label, FontUnit, Font, Scene, Color, Engine, Vector, Input } from "excalibur";
+import {Catboy} from "../catboy";
 
 export class GameScene extends Scene {
-
+    game;
+    dialogueText
     constructor() {
         super();
     }
 
-    onInitialize(){
+    onInitialize(engine){
+        this.game = engine;
+        this.catboy = new Catboy();
+        this.add(this.catboy)
 
+        this.startDialogue()
+        this.dialogueText = new Label({
+            text: 'Time start!',
+            font: new Font({
+                unit: FontUnit.Px,
+                family: 'Determination Mono Web Regular',
+                size: 28,
+                color: Color.White,
+            }),
+            pos: new Vector(250, 100)
+        })
+        
+       
+    }
+    startDialogue() {
+        this.catboy.startDialogue()
     }
 
     onPreUpdate() {
-        
+        this.add(this.dialogueText)
     }
 }
